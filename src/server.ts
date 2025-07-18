5dc398ea346f93a7116dcd3cd319d52c7da2dc2c
Tusharhotbit@123 <tsharma@hotbitinfosoft.com>
2025-07-18 15:49:29 +0530

    replaced all consoles with logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from './app';
+import logger from './utils/logger';
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`🟢 Server running at ${PORT}`);
+  logger.info(`🟢 Server running at ${PORT}`);
 });
